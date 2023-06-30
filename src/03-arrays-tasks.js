15a38fc816bd7cd1deac56d6057a76f6a353c8bf
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-06-30 22:36:17 +0300

    feat: add sortDigitNames

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -329,8 +329,10 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const DIGITS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+  const result = arr.sort((a, b) => DIGITS.indexOf(a) - DIGITS.indexOf(b));
+  return result;
 }
 
 /**
