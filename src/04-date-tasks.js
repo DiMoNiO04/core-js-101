73d2281d6dc98e949bcf1fcc56dff28a4289fe3e
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-07-01 11:18:36 +0300

    feat: add angleBetweenClockHands

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -97,8 +97,16 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hour = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+
+  let angle = Math.abs(30 * (hour % 12) + 0.5 * minutes - 6 * minutes);
+  while (angle > 180) {
+    angle = 360 - angle;
+  }
+
+  return (angle / 180) * Math.PI;
 }
 
 
